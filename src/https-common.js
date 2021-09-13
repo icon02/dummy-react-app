6f596a4992d4f279ee84f1417211592d70c744f2
Nico <nico.layr@gmx.at>
2021-09-13 19:01:37 +0200

    updated https fetch

--- src/https-common.js
+++ src/https-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import iHttps from "https";
 
 const https = axios.create({
-    baseURL: "http://secure.vollicon.com:8080",
+    baseURL: "https://secure.vollicon.com:8443",
     httpsAgent: new iHttps.Agent({
         rejectUnauthorized: false,
     })
